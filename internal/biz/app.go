843c0b2d355cf0ff8471612980c0f77b543e89a2
wangjiabao <840028401@qq.com>
2026-05-31 10:57:50 +0800

    ok

--- internal/biz/app.go
+++ internal/biz/app.go
@@ -5163,11 +5163,11 @@ func (ac *AppUsecase) LandPlaySix(ctx context.Context, address string, req *pb.L
 		}, nil
 	}
 
-	//if landUserUse.OwnerUserId != user.ID {
-	//	return &pb.LandPlaySixReply{
-	//		Status: "非土地用户",
-	//	}, nil
-	//}
+	if landUserUse.OwnerUserId != user.ID {
+		return &pb.LandPlaySixReply{
+			Status: "非土地用户",
+		}, nil
+	}
 
 	if landUserUse.UserId == user.ID {
 		return &pb.LandPlaySixReply{
@@ -8033,7 +8033,7 @@ func (ac *AppUsecase) StakeGetPlay(ctx context.Context, address string, req *pb.
 		}
 
 		return &pb.StakeGetPlayReply{Status: "ok", PlayStatus: 1, Amount: tmpGit}, nil
-	} else {                                                         // 输：下注金额加入池子
+	} else { // 输：下注金额加入池子
 		if err = ac.tx.ExecTx(ctx, func(ctx context.Context) error { // 事务
 			err = ac.userRepo.SetStakeGetPlaySub(ctx, user.ID, float64(req.SendBody.Amount))
 			if nil != err {
